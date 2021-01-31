7d765e86c4c696bad2aada06d62b37b023105bd1
john <john@08liter.com>
2021-01-31 18:25:59 +0900

    MediaStore.Images.Media.DATA Deprecated

--- app/src/main/java/com/rightcode/baseproject/ui/component/imagePicker/ImageFileLoader.kt
+++ app/src/main/java/com/rightcode/baseproject/ui/component/imagePicker/ImageFileLoader.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.provider.MediaStore
 import com.rightcode.baseproject.ui.component.imagePicker.listener.OnImageLoaderListener
 import com.rightcode.baseproject.ui.component.imagePicker.model.Image
-import com.rightcode.baseproject.util.extension.LogD
 import java.io.File
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
@@ -15,7 +14,6 @@ class ImageFileLoader(private val context: Context) {
     private val projection = arrayOf(
         MediaStore.Images.Media._ID,
         MediaStore.Images.Media.DISPLAY_NAME,
-//        MediaStore.Images.Media.DATA,
         MediaStore.Images.Media.BUCKET_ID,
         MediaStore.Images.Media.BUCKET_DISPLAY_NAME
     )
@@ -48,14 +46,11 @@ class ImageFileLoader(private val context: Context) {
                 listener.onFailed(NullPointerException())
                 return
             }
-            // TODO (MediaStore.Images.Media.DATA Deprecated)
             val idColumn = cursor.getColumnIndex(MediaStore.Images.Media._ID)
             val nameColumn = cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME)
-//            val dataColumn = cursor.getColumnIndex(MediaStore.Images.Media.DATA)
             val bucketIdColumn = cursor.getColumnIndex(MediaStore.Images.Media.BUCKET_ID)
             val bucketNameColumn =
                 cursor.getColumnIndex(MediaStore.Images.Media.BUCKET_DISPLAY_NAME)
-            LogD(cursor.isClosed)
             val images = arrayListOf<Image>()
             if (cursor.moveToFirst()) {
                 do {
@@ -65,42 +60,20 @@ class ImageFileLoader(private val context: Context) {
                     }
                     val id = cursor.getLong(idColumn)
                     val name = cursor.getString(nameColumn)
-//                    val path = cursor.getString(dataColumn)
                     val bucketId = cursor.getLong(bucketIdColumn)
                     val bucketName = cursor.getString(bucketNameColumn)
-
-//                    val file = makeSafeFile(path)
-//                    if (file != null && file.exists()) {
-//                        if (name != null && path != null && bucketName != null) {
-                            val uri = ContentUris.withAppendedId(
-                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                                id
-                            )
-                            LogD(uri)
-                            val image = Image(id, name, uri,  bucketId, bucketName)
-                            images.add(image)
-//                        }
-//                    }
-
+                    val uri = ContentUris.withAppendedId(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        id
+                    )
+                    val image = Image(id, name, uri, bucketId, bucketName)
+                    images.add(image)
                 } while (cursor.moveToNext())
             }
 
             cursor.close()
             listener.onImageLoaded(images)
             Thread.interrupted()
-        }
-
-    }
-
-    companion object {
-        private fun makeSafeFile(path: String?): File? {
-            return if (path == null || path.isEmpty()) {
-                null
-            } else try {
-                File(path)
-            } catch (ignored: Exception) {
-                null
-            }
         }
     }
 }
